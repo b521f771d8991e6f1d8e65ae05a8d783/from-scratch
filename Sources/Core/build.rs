0f56c377a1c4b667d63766a16dc68728c6902488
Alexander M. Hipfl <7h3h4qdn@duck.com>
2025-10-31 08:03:20 +0000

    added swift to build.rs

--- Sources/Core/build.rs
+++ Sources/Core/build.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{fmt::format, process::Command};
 
 fn get_build_profile_name() -> String {
     // The profile name is always the 3rd last part of the path (with 1 based indexing).
@@ -11,7 +11,37 @@ fn get_build_profile_name() -> String {
         .to_string()
 }
 
-fn main() {
+fn run_command(command: &str, args: &[&str], directory: &str) {
+    let output = Command::new(command)
+        .args(args)
+        .current_dir(directory)
+        .output()
+        .expect(format!("Failed to run {}", command).as_str());
+
+    if !output.status.success() {
+        println!("cargo:warning=exit status: {:?}", output.status);
+    }
+
+    for line in String::from_utf8_lossy(&output.stdout).lines() {
+        println!("cargo:warning={}", line);
+    }
+    for line in String::from_utf8_lossy(&output.stderr).lines() {
+        println!("cargo:warning={}", line);
+    }
+    if !output.status.success() {
+        panic!("error while running {}", command);
+    }
+}
+
+fn build_swift() {
+    run_command("swift", &["build"], "swift");
+}
+
+fn build_native() {}
+
+fn build_typescript() {}
+
+fn parse_git() {
     {
         let output = Command::new("git")
             .args(&["rev-parse", "HEAD"])
@@ -54,4 +84,11 @@ fn main() {
         let build_profile_name = get_build_profile_name();
         println!("cargo:rustc-env=CARGO_BUILD_PROFILE={}", build_profile_name);
     }
+}
+
+fn main() {
+    build_swift();
+    build_native();
+    build_typescript();
+    parse_git();
 }
